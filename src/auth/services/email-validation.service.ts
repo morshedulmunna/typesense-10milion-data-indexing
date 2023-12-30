21fe317894fa96c4ee00eac6e3eff05f89e95d67
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-30 10:01:23 +0600

    otp validation complete

--- src/auth/services/email-validation.service.ts
+++ src/auth/services/email-validation.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { FastifyReply } from 'fastify';
 import { AuthJwtService } from 'src/libs/auth-jwt.service';
 import { ErrorException } from 'src/libs/errors.exception';
+import { PrismaService } from 'src/libs/prisma.service';
 
 @Injectable()
 export class EmailVerifyService {
-  constructor(private jwt: AuthJwtService) {}
+  constructor(
+    private jwt: AuthJwtService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async verifyEmail(
     OTP: string,
@@ -18,17 +22,19 @@ export class EmailVerifyService {
         process.env.EMAIL_VALIDATION_JWT_SECRET,
       );
 
-      if (decodedData.activationCode !== OTP) {
+      if (decodedData.activationCode !== OTP.toString()) {
         throw new Error('Invalid OTP!');
       }
 
       response.clearCookie('verification_token');
 
+      console.log(decodedData);
+
+      const {} = decodedData;
+
       // TODO:-> Now register user store data in DB
     } catch (error) {
       throw new ErrorException(error);
     }
-
-    // console.log(isVerified);
   }
 }
