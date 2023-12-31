8aa3540ab25590acae7d90f481ef5965e8392445
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-31 11:32:58 +0600

    Db config  with typeORM

---
+++ src/config/postgresDB.config.ts
@@ -0,0 +1,9 @@
+export const postgresConfig = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'password',
+  database: 'typesens-db',
+  synchronize: true,
+};
